783e36d4abcc951c8134c95168e70ea4225152a9
Ahmed Ayadi <72260287+AhmedAyadi94@users.noreply.github.com>
2025-11-14 11:02:09 +0100

    Initialize modules package with imports and exports

---
+++ modules/__init__.py
@@ -0,0 +1,21 @@
+# Agent MCP QA - Modules Package
+
+from .generator import generate_selenium_script, generate_appium_script, generate_postman_collection
+from .analyzer import parse_junit_xml, parse_json_report, detect_anomalies
+from .reporter import generate_html_report, generate_text_report
+from .ci_cd_connector import JenkinsConnector, GitLabConnector
+from .jira_connector import JiraConnector
+
+__all__ = [
+    'generate_selenium_script',
+    'generate_appium_script',
+    'generate_postman_collection',
+    'parse_junit_xml',
+    'parse_json_report',
+    'detect_anomalies',
+    'generate_html_report',
+    'generate_text_report',
+    'JenkinsConnector',
+    'GitLabConnector',
+    'JiraConnector'
+]
